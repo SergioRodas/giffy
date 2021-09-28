e52fd8511f12401dcd53030937eff13bd74d9ef7
sergiorodas <sergiorodasbj@hotmail.com>
2021-09-28 01:57:10 -0300

    Perform the fetch for the gifs and configure the components and paths to create the SPA

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import { useState, useEffect } from 'react';
 import './App.css';
+import ListOfGifs from './components/ListOfGifs';
+import { Link, Route } from 'wouter';
 
-const GIFS = [
-  'https://media3.giphy.com/media/ygNrcLsFcOymvmbXiJ/giphy.gif?cid=ecf05e47wxc7s5brw0d9dazp21q6sk8927bg6767nnosg6vj&rid=giphy.gif&ct=g',
-  'https://media2.giphy.com/media/Ane45y5p7y0HZokoBw/giphy.gif?cid=ecf05e47er2clsdmf4vhubssr6xeduu3lp5q8nq0bmiauvnk&rid=giphy.gif&ct=g'
-]
 
-const DIFFERENT_GIFS = [
-  'https://media0.giphy.com/media/Rr7qHxZLZLCpy/giphy.gif?cid=ecf05e4794vt43b8f4xqmif7giv993fcpgaydpbc70b295zz&rid=giphy.gif&ct=g'
-]
 
 function App() {
-  const [gifs, setGifs] = useState(GIFS)
-
-  useEffect(function() {
-    setGifs(DIFFERENT_GIFS)
-  }, [])
-
   return (
     <div className="App">
       <section className="App-content">
-
-      {
-        gifs.map(singleGif => <img src={singleGif} alt="Messi gif"/>)
-      }
-
-        
+      <h1>App</h1>
+      <Link to="/gif/mono">Gifs de monos</Link>
+      <Link to="/gif/Messi">Gifs de Messi</Link>
+      <Link to="/gif/SouthPark">Gifs de South Park</Link>
+      <Link to="/gif/Rick&Morty">Gifs de Rick & Morty</Link>
+      <Route component={ListOfGifs} path="/gif/:keyword"/>
       </section>
     </div>
   );
