b45fcd33b22e1683af0472307baf916d1613da63
sergiorodas <sergiorodasbj@hotmail.com>
2021-09-30 21:30:40 -0300

    Update getGifs and create getTrendingTermsService

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,18 +1,22 @@
-const apiKey = 'Alq4l76wd1vZGixv0r53G3V1pSmVw5gy'
+import {API_KEY, API_URL} from './settings'
 
-
-
-export default function getGifs({keyword = 'morty'} = {}) {
-  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
-    return fetch(apiURL)
-    .then(res => res.json())
-    .then(response => {
-      const {data} = response
-      const gifs = data.map(image => {
-        const {images, title, id} = image
-        const {url} = images.downsized_medium
-        return {title, id, url}
-      })
-      return gifs
+const fromApiResponseToGifs = apiResponse => {
+  const {data = []} = apiResponse
+  if (Array.isArray(data)) {
+    const gifs = data.map(image => {
+      const {images, title, id} = image
+      const { url } = images.downsized_medium
+      return { title, id, url }
     })
+    return gifs
+  }
+  return []
+}
+
+export default function getGifs ({limit = 25, keyword = 'morty'} = {}) {
+  const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=0&rating=G&lang=en`
+
+  return fetch(apiURL)
+    .then(res => res.json())
+    .then(fromApiResponseToGifs)
 }
---
+++ src/services/settings.js
@@ -0,0 +1,3 @@
+export const API_KEY = 'Alq4l76wd1vZGixv0r53G3V1pSmVw5gy'
+
+export const API_URL = 'https://api.giphy.com/v1'
